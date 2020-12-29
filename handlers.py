9ac0ce857c520f2f23c178075102a84596a0b99e
Jarvis <respectelychka@mail.ru>
2020-12-30 03:02:50 +0400

    add more buttons

--- handlers.py
+++ handlers.py
@@ -5,7 +5,8 @@ from states import Form
 from bot import dp
 
 from stackoverflowAPI import form_message
-from keyboards import stack_keyboard, sort_keyboard, get_question_keyboard
+from keyboards import stack_keyboard, sort_keyboard, get_question_keyboard, change_parameters_button, stack_buttons, \
+    sort_buttons, get_question_button
 
 
 @dp.message_handler(state='*', commands='start')
@@ -14,7 +15,16 @@ async def handler(message: types.Message):
     await message.answer('русский или английский stack?', reply_markup=stack_keyboard)
 
 
-@dp.message_handler(state=Form.choosing_stack,
+@dp.message_handler(lambda message: message.text == change_parameters_button.text,
+                    state=Form.get_questions_with_same_parameters,
+                    content_types=types.ContentTypes.TEXT)
+async def handler(message: types.Message):
+    await Form.choosing_stack.set()
+    await message.answer('русский или английский stack?', reply_markup=stack_keyboard)
+
+
+@dp.message_handler(lambda message: message.text in stack_buttons,
+                    state=Form.choosing_stack,
                     content_types=types.ContentTypes.TEXT)
 async def handler(message: types.Message, state: FSMContext):
     await Form.choosing_sort.set()
@@ -22,7 +32,8 @@ async def handler(message: types.Message, state: FSMContext):
     await message.answer('выберите как отсортировать вопросы', reply_markup=sort_keyboard)
 
 
-@dp.message_handler(state=Form.choosing_sort,
+@dp.message_handler(lambda message: message.text in sort_buttons,
+                    state=Form.choosing_sort,
                     content_types=types.ContentTypes.TEXT)
 async def handler(message: types.Message, state: FSMContext):
     await Form.choosing_tagged.set()
@@ -65,12 +76,16 @@ async def handler(message: types.Message, state: FSMContext):
     tags = user_data['tags']
 
     messages = form_message(stack, sort_type, tags)[:questions_count]
-    for msg in messages:
-        await message.answer(msg)
+    if messages:
+        for msg in messages:
+            await message.answer(msg)
+    else:
+        await message.answer('нет вопросов подходящих под параметры')
     await message.answer('получить вопросы с теми же параметрами?', reply_markup=get_question_keyboard)
 
 
-@dp.message_handler(state=Form.get_questions_with_same_parameters,
+@dp.message_handler(lambda message: message.text == get_question_button.text,
+                    state=Form.get_questions_with_same_parameters,
                     content_types=types.ContentTypes.TEXT)
 async def handler(message: types.Message, state: FSMContext):
     user_data = await state.get_data()
@@ -82,6 +97,14 @@ async def handler(message: types.Message, state: FSMContext):
     tags = user_data['tags']
 
     messages = form_message(stack, sort_type, tags)[:questions_count]
-    for msg in messages:
-        await message.answer(msg)
+    if messages:
+        for msg in messages:
+            await message.answer(msg)
+    else:
+        await message.answer('нет вопросов подходящих под параметры')
     await message.answer('получить вопросы с теми же параметрами?', reply_markup=get_question_keyboard)
+
+
+@dp.message_handler(state='*', content_types=types.ContentTypes.ANY)
+async def send_default_message(message: types.Message):
+    await message.answer('извините, не могу разобрать вашу команду, есть ошибка ввода')
--- keyboards.py
+++ keyboards.py
@@ -2,13 +2,20 @@ from aiogram import types
 
 button_ru = types.KeyboardButton('ru')
 button_en = types.KeyboardButton('en')
+stack_buttons = [button_ru.text, button_en.text]
 stack_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).row(button_ru, button_en)
 
 button_votes = types.KeyboardButton('votes')
 button_activity = types.KeyboardButton('activity')
-sort_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).row(button_votes, button_activity)
+button_creation = types.KeyboardButton('creation')
+button_hot = types.KeyboardButton('hot')
+button_week = types.KeyboardButton('week')
+button_month = types.KeyboardButton('month')
+sort_buttons = [button_votes.text, button_activity.text, button_hot.text, button_week.text, button_month.text]
+sort_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(button_votes).add(button_activity).add(
+    button_creation).row(button_hot, button_week, button_month)
 
-get_question_button = types.KeyboardButton('получить вопросы')
-change_parameters_button = types.KeyboardButton('/start')
-get_question_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).row(get_question_button,
-                                                                            change_parameters_button)
+get_question_button = types.KeyboardButton('получить вопросы c теми же параметрами')
+change_parameters_button = types.KeyboardButton('поменять параметры')
+get_question_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(get_question_button).add(
+    change_parameters_button)
