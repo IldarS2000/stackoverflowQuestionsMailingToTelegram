ef655c64467cd1935554d2808cfd21692975e906
Jarvis <respectelychka@mail.ru>
2020-12-30 03:06:28 +0400

    made request to api more safe

--- stackoverflowAPI.py
+++ stackoverflowAPI.py
@@ -4,26 +4,29 @@ import requests
 
 
 def form_message(stack, sort_type, tags):
-    query = 'https://api.stackexchange.com/2.2/questions?page=1'
-
-    valid_order_arguments = ['desc', 'asc']
-    valid_sort_arguments = ['activity', 'votes', 'creation', 'hot', 'week', 'month']
-
-    stack = 'ru.' if stack == 'ru' else ''
-
-    query += f'&sort={sort_type}&tagged={tags}&site={stack}stackoverflow'
-
-    response = requests.get(query)
-    raw_json = response.text
-
-    processed_json = json.loads(raw_json)
-
-    questions = processed_json['items']
-
     messages = []
-    for index, q in enumerate(questions, 1):
-        message = f'{index}) голосов за вопрос: {q["score"]}\n'
-        message += f'{q["link"]}\n'
-        messages.append(message)
+    try:
+        query = 'https://api.stackexchange.com/2.2/questions?page=1'
+
+        valid_order_arguments = ['desc', 'asc']
+        valid_sort_arguments = ['activity', 'votes', 'creation', 'hot', 'week', 'month']
+
+        stack = 'ru.' if stack == 'ru' else ''
+
+        query += f'&sort={sort_type}&tagged={tags}&site={stack}stackoverflow'
+
+        response = requests.get(query)
+        raw_json = response.text
+
+        processed_json = json.loads(raw_json)
+
+        questions = processed_json['items']
+
+        for index, q in enumerate(questions, 1):
+            message = f'{index}) голосов за вопрос: {q["score"]}\n'
+            message += f'{q["link"]}\n'
+            messages.append(message)
+    except:
+        pass
 
     return messages
